608be2ea80c4798c399455d0d58072b2fe8d1a3b
Jonas <jonascool19@gmail.com>
2026-08-15 20:12:00 +0200

    chore: pin updater to final TS3 parity release

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 RELEASE_REF=v2.1.0
-PINNED_COMMIT=919e543bbc28eeb8a594cd683a788f47de073516
+PINNED_COMMIT=41e3d900912d3db1e8a18cfd51457d76d3f3a3f8
 REPO=jonascool19-pixel/radiobot
 TMP_DIR=$(mktemp -d)
 trap 'rm -rf "$TMP_DIR"' EXIT
