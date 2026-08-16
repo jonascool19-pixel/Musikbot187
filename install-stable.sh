602bfbc06dc34749dd78f98ff5a9d45819420c00
Jonas <jonascool19@gmail.com>
2026-08-16 16:31:44 +0200

    Make stable installer self-contained

--- install-stable.sh
+++ install-stable.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 REPO="https://github.com/jonascool19-pixel/radiobot.git"
 WORKDIR="/tmp/radiobot-install"
 
+apt-get update
+apt-get install -y git
+
 rm -rf "$WORKDIR"
 git clone --depth 1 "$REPO" "$WORKDIR"
 cd "$WORKDIR"
